e11e3e28fab3514e631a5eaec7a53c3ed56855ef
Jannik Indorf <jannik.indorf@allgeier-it.de>
2022-01-27 08:03:37 +0100

    update links in nav

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="header" class="clearfix">
-    <b-navbar type="light" variant="light" fixed="top">
-      <b-navbar-brand>
-        <a href="https://single-spa.js.org" target="_blank">
-          Single spa
-        </a>
+    <b-navbar type="dark" variant="primary" fixed="top">
+      <b-navbar-brand >
+          Single SPA
       </b-navbar-brand>
 
       <b-navbar-nav class="ml-auto">
+        <b-nav-item>
+          <router-link to="/"> Home </router-link>
+          <router-link to="/vue"> Vue-App </router-link>
+          <router-link to="/login"> Login-Page </router-link>
+        </b-nav-item>
         <b-nav-item-dropdown right>
           <template v-slot:button-content>
             <em> {{ username }}</em>
